53069c20e4f39fdfc462d1887e2664c80efaafb8
panmenglin <msx.pan@outlook.com>
2018-12-12 12:58:40 +0800

    滚动处理

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ const order = ['red', 'yellow', 'blue', 'green', 'red']
 const data = {
   data: {
     picDoList: [{
-      autoMatch: null, gmtCreate: 1535886323000, gmtModified: 1542801568000, groupId: '20180901', height: 2240, id: 74744, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13836.jpg', position: null, score: null, size: '2.4MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13836.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13836.jpg', width: 3360
+      scale: 1, autoMatch: null, gmtCreate: 1535886323000, gmtModified: 1542801568000, groupId: '20180901', height: 2240, id: 74744, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13836.jpg', position: null, score: null, size: '2.4MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13836.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13836.jpg', width: 3360
     }, {
-      autoMatch: null, gmtCreate: 1535886321000, gmtModified: 1542801566000, groupId: '20180901', height: 2046, id: 74740, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13829.jpg', position: null, score: null, size: '2.4MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13829.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13829.jpg', width: 3069
+      scale: 1, autoMatch: null, gmtCreate: 1535886321000, gmtModified: 1542801566000, groupId: '20180901', height: 2046, id: 74740, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13829.jpg', position: null, score: null, size: '2.4MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13829.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13829.jpg', width: 3069
     }, {
-      autoMatch: null, gmtCreate: 1535886319000, gmtModified: 1542801564000, groupId: '20180901', height: 2240, id: 74739, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13826.jpg', position: null, score: null, size: '2.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13826.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13826.jpg', width: 3360
+      scale: 1, autoMatch: null, gmtCreate: 1535886319000, gmtModified: 1542801564000, groupId: '20180901', height: 2240, id: 74739, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13826.jpg', position: null, score: null, size: '2.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13826.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13826.jpg', width: 3360
     }, {
-      autoMatch: null, gmtCreate: 1535886317000, gmtModified: 1542801562000, groupId: '20180901', height: 1999, id: 74736, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13822.jpg', position: null, score: null, size: '2.0MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13822.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13822.jpg', width: 2998
+      scale: 1, autoMatch: null, gmtCreate: 1535886317000, gmtModified: 1542801562000, groupId: '20180901', height: 1999, id: 74736, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13822.jpg', position: null, score: null, size: '2.0MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13822.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13822.jpg', width: 2998
     }, {
-      autoMatch: null, gmtCreate: 1535886316000, gmtModified: 1542801560000, groupId: '20180901', height: 1919, id: 74734, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13820.jpg', position: null, score: null, size: '1.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13820.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13820.jpg', width: 2878
+      scale: 1, autoMatch: null, gmtCreate: 1535886316000, gmtModified: 1542801560000, groupId: '20180901', height: 1919, id: 74734, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13820.jpg', position: null, score: null, size: '1.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13820.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13820.jpg', width: 2878
     }, {
-      autoMatch: null, gmtCreate: 1535886315000, gmtModified: 1542801559000, groupId: '20180901', height: 2240, id: 74733, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13818.jpg', position: null, score: null, size: '3.4MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13818.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13818.jpg', width: 3360
+      scale: 1, autoMatch: null, gmtCreate: 1535886315000, gmtModified: 1542801559000, groupId: '20180901', height: 2240, id: 74733, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13818.jpg', position: null, score: null, size: '3.4MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13818.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13818.jpg', width: 3360
     }, {
-      autoMatch: null, gmtCreate: 1535886313000, gmtModified: 1542801556000, groupId: '20180901', height: 2240, id: 74730, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13816.jpg', position: null, score: null, size: '3.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13816.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13816.jpg', width: 3360
+      scale: 1, autoMatch: null, gmtCreate: 1535886313000, gmtModified: 1542801556000, groupId: '20180901', height: 2240, id: 74730, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13816.jpg', position: null, score: null, size: '3.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13816.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13816.jpg', width: 3360
     }, {
-      autoMatch: null, gmtCreate: 1535886310000, gmtModified: 1542801553000, groupId: '20180901', height: 1803, id: 74728, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13811.jpg', position: null, score: null, size: '1.6MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13811.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13811.jpg', width: 2704
+      scale: 1, autoMatch: null, gmtCreate: 1535886310000, gmtModified: 1542801553000, groupId: '20180901', height: 1803, id: 74728, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13811.jpg', position: null, score: null, size: '1.6MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13811.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13811.jpg', width: 2704
     }, {
-      autoMatch: null, gmtCreate: 1535886309000, gmtModified: 1542801552000, groupId: '20180901', height: 1803, id: 74726, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13808.jpg', position: null, score: null, size: '1.5MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13808.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13808.jpg', width: 2704
+      scale: 1, autoMatch: null, gmtCreate: 1535886309000, gmtModified: 1542801552000, groupId: '20180901', height: 1803, id: 74726, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13808.jpg', position: null, score: null, size: '1.5MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13808.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13808.jpg', width: 2704
     }, {
-      autoMatch: null, gmtCreate: 1535886308000, gmtModified: 1542801550000, groupId: '20180901', height: 1803, id: 74724, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13805.jpg', position: null, score: null, size: '1.7MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13805.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13805.jpg', width: 2704
+      scale: 1, autoMatch: null, gmtCreate: 1535886308000, gmtModified: 1542801550000, groupId: '20180901', height: 1803, id: 74724, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13805.jpg', position: null, score: null, size: '1.7MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13805.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13805.jpg', width: 2704
     }, {
-      autoMatch: null, gmtCreate: 1535886305000, gmtModified: 1542801549000, groupId: '20180901', height: 1888, id: 74720, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13802.jpg', position: null, score: null, size: '1.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13802.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13802.jpg', width: 2832
+      scale: 1, autoMatch: null, gmtCreate: 1535886305000, gmtModified: 1542801549000, groupId: '20180901', height: 1888, id: 74720, isIdentified: null, path: '/data/si_board/si_board/20180901/pic/赵强004/ZQ_13802.jpg', position: null, score: null, size: '1.8MB', thumbUrl: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13802.jpg_200', unionId: null, url: 'https://devres.alphalawyer.cn/si_board/20180901/pic/赵强004/ZQ_13802.jpg', width: 2832
     }]
   }
 }
@@ -50,16 +50,15 @@ function debounce(method, time) {
 
 let scrollTop = 0
 
-const imgArr = []
+// const imgArr = []
 
 data.data.picDoList.map(item => {
-  item.scale = 1
   item.x = 0
   item.y = 0
-  imgArr.push({
-    src: item.url + '_1280',
-    scale: 1
-  })
+  // imgArr.push({
+  //   src: item.url + '_1280',
+  //   scale: 1
+  // })
   return true
 })
 
@@ -92,6 +91,14 @@ Component({
       const {data} = this.data
       data[dataset.index].scale = detail.scales
       scale = true
+    },
+    scalee(e) {
+      const {dataset} = e.currentTarget
+      const {data} = this.data
+      data[dataset.index].scale = 2
+      this.setData({
+        data
+      })
     },
     touchstart(e) {
       if (e.touches.length === 1) {
@@ -140,6 +147,13 @@ Component({
     },
     leftScroll(curIndex) {
       const {scrollLeft, data} = this.data
+      if (data[curIndex].scale > 1) {
+        const delta = transformRpx(750) + data[curIndex].x
+        if (delta > 20) {
+          return false
+        }
+      }
+
 
       if (data[curIndex]) {
         data[curIndex].preview = true
@@ -155,15 +169,27 @@ Component({
       }
 
       if (scrollLeft < data.length * transformRpx(750)) {
+        data[curIndex].disabled = false
+
         this.setData({
           scrollLeft: curIndex * transformRpx(750) + transformRpx(750),
           data,
           itemIndex: curIndex + 2
         })
       }
+
+      return true
     },
     rightScroll(curIndex) {
       const {scrollLeft, data} = this.data
+      console.log(data[curIndex].x)
+      if (data[curIndex].scale > 1) {
+        const delta = transformRpx(750) + data[curIndex].x
+        if (delta < transformRpx(750) - 20) {
+          return false
+        }
+      }
+
       if (data[curIndex]) {
         data[curIndex].preview = true
       }
@@ -181,17 +207,17 @@ Component({
       }
 
       if (scrollLeft > 0) {
+        data[curIndex].disabled = false
         this.setData({
           scrollLeft: curIndex * transformRpx(750) - transformRpx(750),
           data,
           itemIndex: curIndex
         })
       }
+
+      return true
     },
     touchmove(e) {
-      console.log(e)
-
-
       if (scale) {
         setTimeout(() => {
           scale = false
@@ -237,17 +263,20 @@ Component({
       const {pageX} = e.touches[0]
       const {dataset} = e.currentTarget
       const {scrollLeft, data} = this.data
+      console.log('l', data[dataset.index].x)
 
       if (data[dataset.index].scale > 1) {
         const delta = transformRpx(750) + data[dataset.index].x
-        if (delta > 20) {
+        if (delta > 0) {
           return false
         }
       }
 
       if (scrollLeft < data.length * transformRpx(750)) {
+        data[dataset.index].disabled = true
         this.setData({
           scrollLeft: scrollLeft + (moveStartX - pageX),
+          data
         })
       }
       return true
@@ -259,14 +288,16 @@ Component({
 
       if (data[dataset.index].scale > 1) {
         const delta = transformRpx(750) + data[dataset.index].x
-        if (delta < transformRpx(750) - 20) {
+        if (delta < transformRpx(750)) {
           return false
         }
       }
 
       if (scrollLeft > 0) {
+        data[dataset.index].disabled = true
         this.setData({
           scrollLeft: scrollLeft - (pageX - moveStartX),
+          data
         })
       }
       return true
