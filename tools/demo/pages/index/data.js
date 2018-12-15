76ce6fd10da4cda053eebc5f5a33153ec2bc2a2c
panmenglin <msx.pan@outlook.com>
2018-12-15 11:19:09 +0800

    图片

--- tools/demo/pages/index/data.js
+++ tools/demo/pages/index/data.js
@@ -1,44 +1,44 @@
 module.exports = {
   data: [{
-    src: '/common/images/Desert 1.jpg',
-    previewSrc: '/common/images/Desert 1.jpg',
-    listSrc: '/common/images/Desert 1.jpg',
-    desc: '图片描述',
-    check: false
+    src: '/common/images/Desert1.jpg', // 原图
+    previewSrc: '/common/images/Desert1_1280.jpg', // 预览大图
+    listSrc: '/common/images/Desert1_200.jpg', // 列表小图
+    desc: '图片描述', // 图片描述
+    check: false // 是否选中
   }, {
-    src: '/common/images/Desert 2.jpg',
-    previewSrc: '/common/images/Desert 2.jpg',
-    listSrc: '/common/images/Desert 2.jpg',
+    src: '/common/images/Desert2.jpg',
+    previewSrc: '/common/images/Desert2_1280.jpg',
+    listSrc: '/common/images/Desert2_200.jpg',
     desc: '图片描述\r\n第二行',
     check: false
   }, {
-    src: '/common/images/Desert 3.jpg',
-    previewSrc: '/common/images/Desert 3.jpg',
-    listSrc: '/common/images/Desert 3.jpg',
+    src: '/common/images/Desert3.jpg',
+    previewSrc: '/common/images/Desert3_1280.jpg',
+    listSrc: '/common/images/Desert3_200.jpg',
+    desc: '纵向图片',
+    check: false
+  }, {
+    src: '/common/images/Desert4.jpg',
+    previewSrc: '/common/images/Desert4_1280.jpg',
+    listSrc: '/common/images/Desert4_200.jpg',
     desc: '',
     check: false
   }, {
-    src: '/common/images/Desert 4.jpg',
-    previewSrc: '/common/images/Desert 4.jpg',
-    listSrc: '/common/images/Desert 4.jpg',
+    src: '/common/images/Desert5.jpg',
+    previewSrc: '/common/images/Desert5_1280.jpg',
+    listSrc: '/common/images/Desert5_200.jpg',
     desc: '',
     check: false
   }, {
-    src: '/common/images/Desert 5.jpg',
-    previewSrc: '/common/images/Desert 5.jpg',
-    listSrc: '/common/images/Desert 5.jpg',
+    src: '/common/images/Desert6.jpg',
+    previewSrc: '/common/images/Desert6_1280.jpg',
+    listSrc: '/common/images/Desert6_200.jpg',
     desc: '',
     check: false
   }, {
-    src: '/common/images/Desert 6.jpg',
-    previewSrc: '/common/images/Desert 6.jpg',
-    listSrc: '/common/images/Desert 6.jpg',
-    desc: '',
-    check: false
-  }, {
-    src: '/common/images/Desert 7.jpg',
-    previewSrc: '/common/images/Desert 7.jpg',
-    listSrc: '/common/images/Desert 7.jpg',
+    src: '/common/images/Desert7.jpg',
+    previewSrc: '/common/images/Desert7_1280.jpg',
+    listSrc: '/common/images/Desert7_200.jpg',
     desc: '',
     check: false
   }]
